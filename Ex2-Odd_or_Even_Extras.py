93f8db45fcfc317648f0ac52aa8b4c7494b42937
NateDee <35738092+NateDee@users.noreply.github.com>
2018-02-28 20:33:55 -0600

    Update Ex2-Odd_or_Even_Extras.py

--- Ex2-Odd_or_Even_Extras.py
+++ Ex2-Odd_or_Even_Extras.py
@@ -2,8 +2,6 @@
 
 #Extras!
 #	1. If the number is a multiple of 4, print out a different message.
-#	2. Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). 
-#		If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
 
 def main():
 	user_number = get_number()
